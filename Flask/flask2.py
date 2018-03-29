162cae8bcc15075fb42e6e147a4772361e3492e1
Carlos Osorno <caosornot@eafit.edu.co>
2018-03-29 00:07:45 -0500

    Avances Flask2

--- Flask/flask2.py
+++ Flask/flask2.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-Bootstrap(app)
  
 @app.route("/")
 def index():
     return render_template('template.html')
 
+@app.route("/historico")
+def historico():
+    return render_template('historico.html')
+
+@app.route("/analisis")
+def analisis():
+    return render_template('analisis.html')
+
+@app.route("/implementacion")
+def implementacion():
+    return render_template('implementacion.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
